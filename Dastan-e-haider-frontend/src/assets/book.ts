f5e146602fe910b18728002eb489df01c5d343a9
rabbab <rabbab94@gmail.com>
2024-11-22 06:46:41 +0500

    book section

---
+++ Dastan-e-haider-frontend/src/assets/book.ts
@@ -0,0 +1,32 @@
+import bookFront1 from '../assets/images/book-front-1.jpg';
+import bookBack1 from '../assets/images/book-back-1.jpg';
+import bookFront2 from '../assets/images/book-front-2.jpg';
+import bookBack2 from '../assets/images/book-back-2.jpg';
+import bookFront3 from '../assets/images/book-front-3.jpg';
+import bookBack3 from '../assets/images/book-back-3.jpg';
+import bookFront4 from '../assets/images/book-front-4.jpg';
+import bookBack4 from '../assets/images/book-back-4.jpg';
+
+
+export const books = [
+    {
+        id: 1,
+        imgFront: bookFront1,
+        imgBack: bookBack1,
+    },
+    {
+        id: 2,
+        imgFront: bookFront2,
+        imgBack: bookBack2,
+    },
+    {
+        id: 3,
+        imgFront: bookFront3,
+        imgBack: bookBack3,
+    },
+    {
+        id: 4,
+        imgFront: bookFront4,
+        imgBack: bookBack4,
+    },
+];
